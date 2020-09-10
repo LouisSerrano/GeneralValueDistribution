d5f426f7f704d71fec191b20b7635f290265a811
LouisSerrano <33746360+LouisSerrano@users.noreply.github.com>
2020-09-10 12:29:31 +0200

    removed the tile coding

--- Toy Experiments/Scripts/oldHorde/valueGVF.py
+++ Toy Experiments/Scripts/oldHorde/valueGVF.py
@@ -4,70 +4,65 @@ import numpy as np
 
 class ValueGVF:
     def __init__(self, agent_info={}):
+        self.num_states = agent_info.get("num_states", 60)
         self.lambd = agent_info.get("lambda", 0.1)
         self.alpha = agent_info.get("alpha", 0.1)
-        self.beta =  0.1*self.alpha
+        self.alpha_h =  0.1*self.alpha
         self.gamma = agent_info.get("gamma", 0.95)
         self.last_gamma = self.gamma
         self.current_gamma = None
         self.last_state = None
         
-        self.num_tilings = agent_info.get("num_tilings", 4)
-        self.num_tiles = agent_info.get("num_tiles", 4)
-        self.tc = MazeTileCoder(num_tilings=self.num_tilings, 
-                                         num_tiles=self.num_tiles)
-        self.iht_size = self.tc.iht_size
-        self.weights = np.zeros(self.iht_size)
-        self.hWeights = np.zeros(self.iht_size)
-        self.eligTrace = np.zeros(self.iht_size)
+        #self.num_tilings = agent_info.get("num_tilings", 4)
+        #self.num_tiles = agent_info.get("num_tiles", 4)
+        #self.tc = MazeTileCoder(num_tilings=self.num_tilings, num_tiles=self.num_tiles)
+        #self.iht_size = self.tc.iht_size
 
-        self.objective_states = agent_info.get("objective_states", [[2,4]])
-        if len(self.objective_states) == 1:
-            self.objective_states = self.objective_states[0]
-            
+        self.weights = np.zeros(self.num_states)
+        self.hWeights = np.zeros(self.num_states)
+        self.eligTrace = np.zeros(self.num_states)
+
         self.transition_gen_ = agent_info.get("transition_gen")
         self.transition_gen_params = agent_info.get("transition_gen_params", {})
         
         self.policy = agent_info.get("policy")
         self.values = np.zeros((6,10))
-        obstacles = [[2,1],[3,1],[4,1],[0,4],[1,4],[5,5],[1,7],[2,7],[2,8],[3,8]]
-        for (i,j) in obstacles:
-            self.values[i,j] = np.nan
+        self.all_state_vectors = np.diag(np.ones(self.num_states))
         
-    def transition_gen(self, state):
-        return self.transition_gen_(self.objective_states, state, self.gamma, self.transition_gen_params)
+    def transition_gen(self, last_state, last_action, state):
+        return self.transition_gen_(self, last_state, last_action, state, self.transition_gen_params)
     
     def get_values(self):
         return self.values
 
+    def set_values(self):
+        self.values = np.dot(self.weights, self.all_state_vectors)
+
     def start(self, state, action, rho = 1.0):
         self.last_state = state
         self.last_action = action
-        self.eligTrace = np.zeros(self.iht_size)
+        self.eligTrace = np.zeros(self.num_states)
         
         return action
     
     def step(self, state, rho = 1.0): 
         # get relevant feature
-        last_i, last_j = self.tc.get_observation(self.last_state)
-        last_state_vector = self.tc.get_state_vector(self.last_state)
-        current_state_vector = self.tc.get_state_vector(state)
+        last_state_vector = self.all_state_vectors[self.last_state]
+        current_state_vector = self.all_state_vectors[state]
         
-        cumulant, gamma = self.transition_gen(state)
+        cumulant, gamma = self.transition_gen(self.last_state, None, state)
         self.cumulant = cumulant
         
-        ## retrieve the values
+        # retrieve the values
         self.last_state_value = np.sum(self.weights*last_state_vector)
         self.current_state_value = np.sum(self.weights*current_state_vector)
         
-        ##GTD lambda updates
+        # GTD lambda updates
         self.delta = self.cumulant + (gamma * self.current_state_value - self.last_state_value)
         self.eligTrace = rho*(self.lambd * self.last_gamma * self.eligTrace + last_state_vector)
         self.weights += self.alpha *(self.delta * self.eligTrace - gamma*(1-self.lambd)*np.dot(self.eligTrace, self.hWeights)*current_state_vector)
-        self.hWeights += self.beta * (self.delta*self.eligTrace - np.sum(self.hWeights*last_state_vector)*last_state_vector)
-        
-        self.values[last_i, last_j] = np.sum(self.weights*last_state_vector)
-        
+        self.hWeights += self.alpha_h * (self.delta*self.eligTrace - np.sum(self.hWeights*last_state_vector)*last_state_vector)
+
         self.last_gamma = gamma
         self.prediction = self.last_state_value
         
@@ -75,28 +70,29 @@ class ValueGVF:
         
     def end(self, state, rho = 1.0): 
         # get relevant feature
-        last_i, last_j = self.tc.get_observation(self.last_state)
-        last_state_vector = self.tc.get_state_vector(self.last_state)
-        current_state_vector = self.tc.get_state_vector(state)
-        
-        cumulant, gamma = self.transition_gen(state)
+        # get relevant feature
+        last_state_vector = self.all_state_vectors[self.last_state]
+        current_state_vector = self.all_state_vectors[state]
+
+        cumulant, gamma = self.transition_gen(self.last_state, None, state)
+
         self.cumulant = cumulant
         
-        ## retrieve the values
+        # retrieve the values
         self.last_state_value = np.sum(self.weights*last_state_vector)
         self.current_state_value = np.sum(self.weights*current_state_vector)
         
-        ##GTD lambda updates
+        # GTD lambda updates
         self.delta = self.cumulant + (gamma * self.current_state_value - self.last_state_value)
         self.eligTrace = rho*(self.lambd * self.last_gamma * self.eligTrace + last_state_vector)
         self.weights += self.alpha *(self.delta * self.eligTrace - gamma*(1-self.lambd)*np.dot(self.eligTrace, self.hWeights)*current_state_vector)
-        self.hWeights += self.beta * (self.delta*self.eligTrace - np.sum(self.hWeights*last_state_vector)*last_state_vector)
-        
-        self.values[last_i, last_j] = np.sum(self.weights*last_state_vector)
-        
+        self.hWeights += self.alpha_h * (self.delta*self.eligTrace - np.sum(self.hWeights*last_state_vector)*last_state_vector)
+
         self.last_gamma = gamma
         self.prediction = self.last_state_value
-        
-        
+
+        # update state values
+        self.set_values()
+
         self.last_gamma = gamma
         self.prediction = self.last_state_value    
